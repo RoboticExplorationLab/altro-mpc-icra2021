a08c5581e2a5a4e59e66534d837ac2731c76ce58
Rianna Jitosho <rjitosho@stanford.edu>
2020-10-16 23:02:22 -0700

    grasp benchmarks

--- benchmarks/grasp_optimization/altro_MPC.jl
+++ benchmarks/grasp_optimization/altro_MPC.jl
@@ -8,14 +8,14 @@ include("src/grasp_model.jl") # generates model o, x0, xf
 ex = 2
 include("settings$ex.jl") # creates o, x0, xf
 
-# indices for convenience
+# Indices for convenience
 pos_ind = 1:Int(n/2)
 vel_ind = 1+Int(n/2):n
 u_ind = n .+ (1:m)
 F1_ind = n .+ (1:Int(m/2))
 F2_ind = n .+ (1+Int(m/2):m)
 
-# objective
+# Objective
 Q = 1.0e-3*Diagonal(@SVector ones(n))
 Qf = 10.0*Diagonal(@SVector ones(n))
 R = 1.0*Diagonal(@SVector ones(m))
@@ -28,6 +28,7 @@ conSet = ConstraintList(n,m,N)
 goal = GoalConstraint(SVector{n}(xf))
 add_constraint!(conSet, goal, N)
 
+# Stage Constraints
 include("src/new_constraints.jl")
 for i = 1:N-1
     global model, u_ind, F1_ind, F2_ind, n, m
@@ -67,8 +68,8 @@ U0 = [u0 for k = 1:N-1]
 initial_controls!(prob, U0)
 rollout!(prob);
 
+# Solver
 using Altro
-# for example 2, solves in 50 ms
 opts = SolverOptions(
     verbose = 1,
     projected_newton_tolerance=1e-4,
@@ -78,44 +79,48 @@ opts = SolverOptions(
     constraint_tolerance=1e-4
 )
 
-# solve
+# Solve
 altro = ALTROSolver(prob, opts)
 solve!(altro)
 
-# extract results
-X_cold = states(altro)
-U_cold = controls(altro)
+# Extract Results
+X_cold = states(altro) # for setting reference X trajectory
+U_cold = controls(altro) # for setting reference U trajectory
 
 ## RUN MPC
 include("src/mpc_helpers_altro.jl")
 
-# Initial Conditions
+# Setup
+num_iters = 15 # number of MPC iterations
+hor = 10 # length of the MPC horizon in number of steps
 x_curr = X_cold[1] # Current state (x, v)
 u_curr = U_cold[1] # Controls (u) applied at this instant
-hor = 10 # length of the MPC horizon in number of steps
+X_warm = X_cold[1:hor] # X trajectory for warmstarting
+U_warm = U_cold[1:hor-1] # U trajectory for warmstarting
 
-# Set up problem
-obj, conSet = altro_mpc_setup(o, X_cold[1:hor], U_cold[1:hor], hor)
+obj, conSet = altro_mpc_setup(o, X_cold[1:hor], U_cold[1:hor-1], hor)
 prob = Problem(o, obj, X_cold[hor], dt*(hor-1), x0=X_cold[1], constraints=conSet)
+initial_controls!(prob, U_cold[1:hor-1])
+rollout!(prob);
 
+# Arrays for results
 altro_times = []
 altro_states = []
 altro_controls = []
-num_iters = N - hor - 2
+push!(altro_states, x_curr)
 
 for iter in 1:num_iters
-    global x_curr, u_curr, prob, altro, opts
-    local X_warm, U_warm, b
+    global x_curr, u_curr, prob, altro, opts, X_warm, U_warm
 
     # Propagate the physics forward to the next timestep
     x_curr = noisy_discrete_dynamics(o, x_curr, u_curr, dt)
 
-    # Construct the warm-started state and control arrays
-    X_warm = [[x_curr]; X_cold[2 + iter:hor + iter]]
-    U_warm = U_cold[1 + iter:hor + iter]
+    # Set trajectory to track
+    X_ref = [[x_curr]; X_cold[iter .+ (2:hor)]]
+    U_ref = U_cold[iter .+ (1:hor-1)]
 
     # Update problem
-    altro_mpc_update!(prob, o, X_warm, U_warm, hor, iter)
+    altro_mpc_update!(prob, o, X_ref, U_ref, hor, iter, X_warm, U_warm)
 
     # Solve
     altro = ALTROSolver(prob, opts)
@@ -123,15 +128,16 @@ for iter in 1:num_iters
     solve!(altro)
     b = benchmark_solve!(altro, samples=3, evals=1)
 
-    # extract control
-    U_new = controls(altro)
-    u_curr = U_new[1]
+    # Extract trajectory for warmstarting
+    X_warm=states(altro)
+    U_warm=controls(altro)
+    u_curr = controls(altro)[1]
 
-    # printouts
+    # Printouts
     println("Iter $iter: $(round(median(b).time/1e6, digits=2)) ms")
     println("Max violation: $(TrajectoryOptimization.max_violation(altro))")
 
-    # push values
+    # Push values
     append!(altro_times, b.times/1e6)
     push!(altro_states, x_curr)
     push!(altro_controls, u_curr)
