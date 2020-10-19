ded91c674df37717ab90bc99afb5408d42f555ee
Rianna Jitosho <rjitosho@stanford.edu>
2020-10-19 14:42:26 -0700

    Update histogram for grasp opt

--- benchmarks/grasp_optimization/grasp_benchmark.jl
+++ benchmarks/grasp_optimization/grasp_benchmark.jl
@@ -9,9 +9,9 @@ using JLD2
 @save string(@__DIR__,"/grasp_timing_data.jld2") altro_times ecos_times
 
 using Plots
-bins = collect(0:5:30)
+bins = collect(4:2:22)
 histogram(altro_times, bins=bins, fillalpha=.5, label="ALTRO")
 histogram!(ecos_times, bins=bins, fillalpha=.5, label="ECOS")
-xlabel!("Solve Times (ms)")
-title!("Grasp MPC Solve Times")
+xlabel!("Solve Time (ms)")
+ylabel!("Counts")
 png(string(@__DIR__,"/grasp_hist.png"))
