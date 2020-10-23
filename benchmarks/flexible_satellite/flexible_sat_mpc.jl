a562793557450214cbabd5a91fa9ec530995db58
Kevin Tracy <ktracy@stanford.edu>
2020-10-23 16:25:57 -0700

    Updated flexible sat example

--- benchmarks/flexible_satellite/flexible_sat_mpc.jl
+++ benchmarks/flexible_satellite/flexible_sat_mpc.jl
@@ -1,5 +1,6 @@
 import Pkg; Pkg.activate(joinpath(@__DIR__,"..")); Pkg.instantiate()
 
+include(joinpath(dirname(dirname(@__FILE__)),"plotting.jl"))
 
 using SparseArrays, Random
 using TrajectoryOptimization
@@ -34,6 +35,7 @@ function mat_from_vec(a)
     return A
 end
 function OSQP_postprocess_mpc(results,N,nx,nu)
+    """Take the OSQP solver results and convert to states and controls"""
     X = zeros(nx,N+1)
     U = zeros(nu,N)
     x_vec = results.x[1:(N+1)*nx]
@@ -52,6 +54,7 @@ function OSQP_postprocess_mpc(results,N,nx,nu)
 end
 
 function c2d(A,B,dt)
+    """Continuous to discrete for linear systems"""
     n = size(A,1)
     p = size(B,2)
 
@@ -64,6 +67,8 @@ function c2d(A,B,dt)
 end
 
 function generate_AB()
+    """Create A and B for flexible satellite"""
+
     # inertia matrix
     J = diagm([1;2;3])
 
@@ -122,21 +127,21 @@ function generate_AB()
 end
 
 
-function test_mpc()
-# Random.seed!(1234)
+function run_flexsat_mpc()
+
+# get linear system
 Ad, Bd = generate_AB()
 A = copy(Ad)
 B = copy(Bd)
 N = 80
 n,m = size(B)
 
-
+# cost function
 Q = Diagonal(10*ones(n))
 R = Diagonal(0.1*ones(m))
-
 Qf = copy(Q)
 
-
+# control limits
 ū = .01*ones(m)
 
 xf = zeros(n)
@@ -144,7 +149,6 @@ x0 = [.1;.1;.1;zeros(n-3)]
 dt = 0.1 # doesn't matter, just needs to be non-zero
 model = RD.LinearModel(A, B;dt = .1)
 objective = LQRObjective(Q, R, Qf, xf, N)
-
 constraints = ConstraintList(n, m, N)
 bound = BoundConstraint(n, m, u_min=-ū, u_max=ū)
 add_constraint!(constraints, bound, 1:N)
@@ -209,12 +213,14 @@ u = [ueq; uineq]
 m = OSQP.Model()
 # setup problem
 OSQP.setup!(m; P=P, q=q, A=A, l=l, u=u, eps_abs = 1e-6, eps_rel = 1e-6, eps_prim_inf = 1e-6, eps_dual_inf = 1e-6)
-# OSQP.setup!(m; P=P, q=q, A=A, l=l, u=u, eps_abs = 1e-7)
+
 # solve
 results = OSQP.solve!(m)
 
 X_osqp,U_osqp = OSQP_postprocess_mpc(results,N,nx,nu)
 
+# this commented out block is good for verifying OSQP and ALTRO results are
+# the same
 # mat"
 # figure
 # hold on
@@ -227,11 +233,9 @@ X_osqp,U_osqp = OSQP_postprocess_mpc(results,N,nx,nu)
 # hold on
 # plot($X_altro')
 # hold off"
-#
-# @infiltrate
-# error()
 
 
+# number of MPC runs
 mpc_iterations = 45
 
 osqp_times = zeros(mpc_iterations)
@@ -261,8 +265,7 @@ for i = 1:mpc_iterations
       results = OSQP.solve!(m)
       osqp_times[i] = results.info.solve_time*1e3
 
-      # @infiltrate
-      # error()
+
 
 end
 
@@ -271,35 +274,28 @@ X_osqp,U_osqp = OSQP_postprocess_mpc(results,N,nx,nu)
 return osqp_times, altro_times, X_osqp, states(solver)
 end
 
+# run the mpc simulation 10 times
 sim_trials = 10
 osqp_time_mat = zeros(45,sim_trials)
 altro_time_mat = zeros(45,sim_trials)
-
 for i = 1:sim_trials
-    osqp_times, altro_times, X_osqp, X_altro = test_mpc()
-    altro_times = altro_times[11:end]
-    osqp_times = osqp_times[11:end]
+    osqp_times, altro_times, X_osqp, X_altro = run_flexsat_mpc()
+    altro_times = altro_times[1:end]
+    osqp_times = osqp_times[1:end]
     altro_time_mat[:,i] = altro_times
     osqp_time_mat[:,i] = osqp_times
 end
 
-
-osqp_quants = zeros(size(osqp_time_mat,1),3)
-lo = zeros(size(osqp_time_mat,1))
-up = zeros(size(osqp_time_mat,1))
+# get median solve times for each
 osqp_med = zeros(size(osqp_time_mat,1))
 altro_med = zeros(size(osqp_time_mat,1))
 for i = 1:size(osqp_time_mat,1)
-    # osqp_quants[i,1],osqp_quants[i,2],osqp_quants[i,3] =
-    # quantile(osqp_time_mat[i,:],[0.25 0.5 0.75])
-    # lo[i] = minimum(osqp_time_mat[i,:])
-    # up[i] = maximum(osqp_time_mat[i,:])
     osqp_med[i] = median(osqp_time_mat[i,:])
     altro_med[i] = median(altro_time_mat[i,:])
 end
 
 function vec_from_mat(mat)
-    #vector of vectors from matrix of column vectors
+    """vector of vectors from matrix of column vectors"""
 
     s = size(mat)
     if length(s) == 3
@@ -324,12 +320,7 @@ function vec_from_mat(mat)
     return V
 end
 
-# q1 = osqp_quants[:,1]
-# q2 = osqp_quants[:,2]
-# q3 = osqp_quants[:,3]
-# coords = Coordinates([],[])
-# opts=(@pgf {})
-# y = 0
+
 width = 1
 
 Q = vec_from_mat(osqp_time_mat')
@@ -368,67 +359,4 @@ p = @pgf TikzPicture(
         Legend("ALTRO","OSQP")
     ))
 
-print("ya")
-
-mat"
-figure
-hold on
-plot($altro_time_mat,'r')
-plot($osqp_time_mat,'b')
-hold off
-"
-
-mat"
-figure
-hold on
-boxplot($altro_time_mat')
-hold off
-"
-# p =@pgf PlotInc(
-#     {
-#         opts...,
-#         "solid",
-#         "line width"="2pt",
-#         "forget plot",
-#         "boxplot prepared" = {
-#             "draw direction"="y",
-#             "draw position"=y,
-#             "lower whisker"=lo,
-#             "lower quartile"=q1,
-#             "median"=q2,
-#             "upper quartile"=q3,
-#             "upper whisker"=up,
-#             "box extend"=width,
-#         },
-#         "mark options"={scale=.5},
-#     }
-# )
-# altro_avg = zeros(length(altro_times))
-# osqp_avg = zeros(length(osqp_times))
-#
-# for i = 1:length(altro_avg)
-#       altro_avg[i] = mean(altro_times[max(1,i-9):i])
-#       osqp_avg[i] = mean(osqp_times[max(1,i-9):i])
-# end
-
-
-# function comp_plot(xs, times_altro, times_osqp; kwargs...)
-#     times_altro *= 1000
-#     times_osqp *= 1000
-#     avg_altro = mean.(eachrow(times_altro))
-#     std_altro = std.(eachrow(times_altro))
-#     avg_osqp = mean.(eachrow(times_osqp))
-#     std_osqp = std.(eachrow(times_osqp))
-#     p = plot(ylabel="time (ms)"; kwargs...)
-#     plot!(xs, avg_altro, yerr=std_altro, markerstrokecolor=:auto, label="ALTRO")
-#     plot!(xs, avg_osqp, yerr=std_osqp, markerstrokecolor=:auto, label="OSQP")
-#     return p
-# end
-#
-# comp_plot(1:45, altro_time_mat, osqp_time_mat, xlabel="MPC Steps")
-#
-
-
-
-# using JLD2
-# @save joinpath(dirname(@__FILE__),"flexible_satellite_data.jld2") altro_times altro_avg osqp_times osqp_avg
+pgfsave(joinpath(IMAGE_DIR, "flexible_sat_comp.tikz"), p, include_preamble=false)
