c79bd571b2a70a81bad75867d5ef591643ef56df
Daniel Neamati <danineamati@gmail.com>
2020-10-23 11:03:56 -0700

    Convex and Altro problem generation with same header

--- benchmarks/rocket_landing/src/make_problem_ALTRO.jl
+++ benchmarks/rocket_landing/src/make_problem_ALTRO.jl
@@ -7,7 +7,7 @@ include("utils.jl")
 include("general_socp.jl")
 
 function make_problem_ALTRO_COLD(r::Rocket, obj_opts::ObjectiveOptions,
-                    t_opts::TrajectoryOptionsCOLD, out_opts::OutputOptions;
+                    t_opts::TrajectoryOptions, out_opts::OutputOptions;
                     includeGoal::Bool = true, ground_level::Float64 = 0.0)
     println("Chose ALTRO with Cold Start")
 
@@ -29,7 +29,7 @@ function make_problem_ALTRO_COLD(r::Rocket, obj_opts::ObjectiveOptions,
     if includeGoal
         # Goal Constraint that the rocket must reach the landing site.
         goal = GoalConstraint(t_opts.xf)
-        add_constraint!(conSet, goal, t_opts.N)
+        TrajectoryOptimization.add_constraint!(conSet, goal, t_opts.N)
         if out_opts.verbose
             println("Goal Constraint Set")
         end
@@ -40,7 +40,7 @@ function make_problem_ALTRO_COLD(r::Rocket, obj_opts::ObjectiveOptions,
     # This constraint can also be a glidescope constraint.
     bnd = BoundConstraint(n,m, x_min=[-Inf, -Inf, ground_level,
                                       -Inf, -Inf, -Inf])
-    add_constraint!(conSet, bnd, 1:t_opts.N-1)
+    TrajectoryOptimization.add_constraint!(conSet, bnd, 1:t_opts.N-1)
     if out_opts.verbose
         println("Ground Constraint Set")
     end
@@ -48,7 +48,7 @@ function make_problem_ALTRO_COLD(r::Rocket, obj_opts::ObjectiveOptions,
     # Norm Constrant that reflects the max thrust the rocket can provide
     maxT = NormConstraint(n, m, r.u_max,
                         TrajectoryOptimization.SecondOrderCone(), :control)
-    add_constraint!(conSet, maxT, 1:t_opts.N-1)
+    TrajectoryOptimization.add_constraint!(conSet, maxT, 1:t_opts.N-1)
     if out_opts.verbose
         println("Max Thrust Constraint Set")
     end
@@ -60,14 +60,15 @@ function make_problem_ALTRO_COLD(r::Rocket, obj_opts::ObjectiveOptions,
     thetacRocket = SVector{3}([0; 0; maxTAalpha])
     maxTA = NormConstraint2(n, m, thetaARocket, thetacRocket,
                         TrajectoryOptimization.SecondOrderCone(), :control)
-    add_constraint!(conSet, maxTA, 1:t_opts.N-1)
+    TrajectoryOptimization.add_constraint!(conSet, maxTA, 1:t_opts.N-1)
     if out_opts.verbose
         println("Max Thrust Angle Constraint Set")
     end
 
     # Package the objective and constraints into a "problem" type
     tf = t_opts.N * t_opts.dt
-    prob = Problem(model, obj, t_opts.xf, tf, x0=t_opts.x0, constraints=conSet)
+    prob = TrajectoryOptimization.Problem(model, obj, t_opts.xf, tf,
+                                            x0=t_opts.x0, constraints=conSet)
     if out_opts.verbose
         println("Problem Made with tf = $tf")
     end
@@ -133,6 +134,7 @@ function make_problem_ALTRO_WARM(r::Rocket, obj_opts::ObjectiveOptions,
     # This constraint can also be a glidescope constraint.
     bnd = BoundConstraint(n,m, x_min=[-Inf, -Inf, ground_level,
                                       -Inf, -Inf, -Inf])
+    TrajectoryOptimization.add_constraint!(conSet, bnd, 1:(t_opts.Horizon-1))
     if out_opts.verbose
         println("Ground Constraint Set")
     end
@@ -140,7 +142,7 @@ function make_problem_ALTRO_WARM(r::Rocket, obj_opts::ObjectiveOptions,
     # Norm Constrant that reflects the max thrust the rocket can provide
     maxT = NormConstraint(n, m, r.u_max,
                         TrajectoryOptimization.SecondOrderCone(), :control)
-    add_constraint!(conSet, maxT, 1:(t_opts.Horizon-1))
+    TrajectoryOptimization.add_constraint!(conSet, maxT, 1:(t_opts.Horizon-1))
     if out_opts.verbose
         println("Max Thrust Constraint Set")
     end
@@ -152,14 +154,14 @@ function make_problem_ALTRO_WARM(r::Rocket, obj_opts::ObjectiveOptions,
     thetacRocket = SVector{3}([0; 0; maxTAalpha])
     maxTA = NormConstraint2(n, m, thetaARocket, thetacRocket,
                         TrajectoryOptimization.SecondOrderCone(), :control)
-    add_constraint!(conSet, maxTA, 1:(t_opts.Horizon-1))
+    TrajectoryOptimization.add_constraint!(conSet, maxTA, 1:(t_opts.Horizon-1))
     if out_opts.verbose
         println("Max Thrust Angle Constraint Set")
     end
 
     # Goal Constraint that the rocket must reach the landing site.
     goal = GoalConstraint(xTrack[end])
-    add_constraint!(conSet, goal, t_opts.Horizon)
+    TrajectoryOptimization.add_constraint!(conSet, goal, t_opts.Horizon)
     if out_opts.verbose
         println("Goal Constraint Set at $xf")
     end
